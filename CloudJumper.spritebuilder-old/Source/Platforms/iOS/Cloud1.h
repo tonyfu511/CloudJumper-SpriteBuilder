4d907a709cce5b7a9eecf877bacbd9a277b4c5d2
tonyfu511 <tonyfu511@gmail.com>
2015-04-27 14:42:29 -0400

    2

---
+++ CloudJumper.spritebuilder-old/Source/Platforms/iOS/Cloud1.h
@@ -0,0 +1,15 @@
+//
+//  Cloud1.h
+//  CloudJumper
+//
+//  Created by Tony Fu on 4/10/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Cloud1 : CCNode
+
+- (void)setupRandomPosition;
+
+@end
---
+++ CloudJumper.spritebuilder-old/Source/Platforms/iOS/TopNail.h
@@ -0,0 +1,13 @@
+//
+//  TopNail.h
+//  CloudJumper
+//
+//  Created by Tony Fu on 4/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface TopNail : CCSprite
+
+@end
